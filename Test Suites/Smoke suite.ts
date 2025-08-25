0317b8c4b6cad99fe14c165f5586015f95ebc676
adeshmukh <adeshmukh@ADESHMUKH11.priv.life-south.com>
2025-08-25 09:50:58 -0400

    New test case an suit added

---
+++ Test Suites/Smoke suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Smoke suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>964f681b-25d5-4228-accb-27d745e2361d</testSuiteGuid>
+</TestSuiteEntity>
